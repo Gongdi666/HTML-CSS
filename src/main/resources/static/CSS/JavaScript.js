5c416adbf0d70ae43d51fc7b8e6586d88140231e
yumamiyaji <yumamiyaji@localhost>
2020-03-09 18:45:25 +0900

    HTML CSS確認よう

---
+++ src/main/resources/static/CSS/JavaScript.js
@@ -0,0 +1,56 @@
+/**
+ *
+ **/
+
+   
+ /**
+  * gamen2を消す
+  */ 
+    $('.gamen2').hide();
+
+/**
+ * gamen2をつける
+ */
+	$('.shinki').click(function(){
+		$('.gamen2').show();
+	})
+
+/**
+ * gamen3を消す
+ */
+	$('.kensaku').click(function(){
+		$('.gamen2').hide();
+	})
+
+/**
+ * gamen3を消す
+ */
+	$('.gamen3').hide();
+/**
+ * gamen3つける
+ */
+	$('.kensaku_button').click(function(){
+		$('.gamen3').show();
+	})
+/**
+ * 更新
+ */
+	$('.button1').click(function() {
+       alert('更新してもよろしいですか？');
+    })
+ 
+    
+ 
+    
+  
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+   
